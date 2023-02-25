72ddd389c759285900c75f78b007c2eab4e7e30b
NiteshTembe <123352437+NiteshTembe@users.noreply.github.com>
2023-02-25 23:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
+// title for head tag
 document.title="Promise Task 1"
-
+// function to fetch all fish data using givrn api
 async function getFishData(){
     try{
         let res = await fetch("https://www.fishwatch.gov/api/species")
         let data = await res.json()
-     //   console.log(data[0]["Image Gallery"])
-     //   console.log(data)
         showFishData(data)
     }
     catch(err){
         console.log(err)
     }
 }
-
+// h1 tag with text "Fish Species created" 
 const title= document.createElement("h1")
 title.classList.add("text-center")
 title.id="title"
 const titletext = document.createTextNode("Fish Species")
 title.appendChild(titletext)
 document.body.appendChild(title)
+// main div with class container & div with class row 
 const container = document.createElement("div")
 container.classList.add("container")
 const rowdiv = document.createElement("div")
@@ -26,13 +26,15 @@ rowdiv.classList.add("row")
 rowdiv.id="fishdatadiv"
 container.appendChild(rowdiv)
 document.body.appendChild(container)
-
+// function is called here to fetch data
 getFishData()
 
+//thisfunction is used to show data using bootstrap card in document
 function showFishData(data){
     let fishdatadiv = document.getElementById("fishdatadiv")
-    console.log(data)
-    data.map((element)=>{
+ //   console.log(data)
+    data.map((element)=>{  //to map each data of array
+        // each data of aaray data is added inhtml document 
         fishdatadiv.innerHTML+=`
             <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-4">
             <div class="card h-100">
